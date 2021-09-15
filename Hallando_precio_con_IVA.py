85b740f609928d21409c1d2baa332b5e1ff747dd
Kevin <andrestorrecilla.14@gmail.com>
2021-09-15 18:00:39 -0500

    Terminando el punto 4

---
+++ Hallando_precio_con_IVA.py
@@ -0,0 +1,38 @@
+def precio (Precio_producto):
+
+    if str(Precio_producto).isdigit() == True:
+        Precio_producto = int(Precio_producto)
+    
+    if str(Precio_producto).isdigit() == False:
+        Precio_producto = float(Precio_producto)
+
+    Precio_iva = Precio_producto * 0.19
+    
+    if Precio_iva % 1 == 0:
+        Precio_iva = int(Precio_iva)
+        
+    
+    if Precio_iva % 1 != 0:
+        Precio_iva = float(Precio_iva)
+        "{:.2f}".format(Precio_iva)
+
+    Precio_producto_final = Precio_producto + Precio_iva
+
+    if Precio_producto_final % 1 == 0:
+        Precio_producto_final = int(Precio_producto_final)
+        print(f"El precio del producto con IVA es: {Precio_producto_final} pesos")
+    
+    if Precio_producto_final % 1 != 0:
+        Precio_producto_final = float(Precio_producto_final)
+        print(f"El precio del producto con IVA es: " + "{:.2f}".format(Precio_producto_final), " pesos")
+
+    if Precio_iva % 1 != 0:
+        Precio_iva = float(Precio_iva)
+        print(f"El precio del IVA es: " + "{:.2f}".format(Precio_iva), " pesos")
+
+    if Precio_iva % 1 == 0:
+        Precio_iva = int(Precio_iva)
+        print(f"El precio del IVA es: {Precio_iva} pesos")
+
+precio(input("Digite el precio del producto \n"))
+
