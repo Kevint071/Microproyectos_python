1834224452632c1133de9bf33e3718a51d3c64da
Kevin <andrestorrecilla.14@gmail.com>
2021-09-15 21:32:56 -0500

    Mejorando el punto 3

--- Promedio_3_notas.py
+++ Promedio_3_notas.py
@@ -5,11 +5,28 @@ def notas ():
     for i in range (1, 4):
         print("Digite la nota", i)
         nota = input ()
-        lista_de_notas.append(nota)    
+        lista_de_notas.append(nota)
+        
     Nota_1, Nota_2, Nota_3 = lista_de_notas
-    Nota_1 = int(Nota_1)
-    Nota_2 = int(Nota_2)
-    Nota_3 = int(Nota_3)
+
+    if Nota_1.isdigit() == False:
+        Nota_1 = float(Nota_1)
+    
+    if Nota_2.isdigit() == False:
+        Nota_2 = float(Nota_2)
+
+    if Nota_3.isdigit() == False:
+        Nota_3 = float(Nota_3)
+    
+    if str(Nota_1).isdigit() == True:
+        Nota_1 = int(Nota_1)
+
+    if str(Nota_2).isdigit() == True:
+        Nota_2 = int(Nota_2)
+    
+    if str(Nota_3).isdigit() == True:
+        Nota_3 = int(Nota_3)
+
     Suma = Nota_1 + Nota_2 + Nota_3
     Promedio = Suma / 3
 
@@ -24,5 +41,8 @@ def notas ():
         notas()
     
     if Nota_1 >= 0 and Nota_1 <= 10 and Nota_2 >= 0 and Nota_2 <= 10 and Nota_3 >= 0 and Nota_3 <= 10:
-        print(f"El promedio de sus notas es {Promedio}")
+        if Promedio % 1 == 0:
+            print(f"El promedio de sus nodas es: {Promedio}")
+        if Promedio % 1 != 0:
+            print(f"El promedio de sus notas es: ""{:.2f}".format(Promedio))
 notas()
